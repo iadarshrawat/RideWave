44810b95005aa5c8d0c2116deca53be053a07b21
Adarsh Rawat <00adarshrawat00@gmail.com>
2024-12-18 11:52:32 +0530

    adding register

--- Backend/app.js
+++ Backend/app.js
@@ -2,13 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 const cors = require('cors')
-
+const userRoutes = require('./routes/user.routes');
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res)=>{
     res.send("helllo wrold")
 })
+
+
+app.use('/users', userRoutes);
 
 
 module.exports = app;
